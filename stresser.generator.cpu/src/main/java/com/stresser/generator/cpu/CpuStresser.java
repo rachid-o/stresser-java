87b26466417bf1b46ecaa8fbef079049bf88257e
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2018-05-10 11:33:28 +0200

    break the infinitive loop when stresser is finished

--- stresser.generator.cpu/src/main/java/com/stresser/generator/cpu/CpuStresser.java
+++ stresser.generator.cpu/src/main/java/com/stresser/generator/cpu/CpuStresser.java
@@ -7,9 +7,11 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+import static java.lang.String.format;
+
 public class CpuStresser {
 
-    static final int timeout = 5;
+    static final int timeout = 3;
     static final int delay = 0;
 
 
@@ -19,7 +21,7 @@ public class CpuStresser {
 
     public void start() {
         println(getName() + " Start stressing the CPU");
-        println(String.format("Start stressing CPU for %ds after waiting for %ds", timeout, delay));
+        println(format("Start stressing CPU for %ds after waiting for %ds", timeout, delay));
 
         try {
             stress();
@@ -31,23 +33,35 @@ public class CpuStresser {
     private void stress() throws InterruptedException {
         int procsAvail = Runtime.getRuntime().availableProcessors();
 
-        println(String.format("Create %d concurrent tasks", procsAvail));
+        println(format("Create %d concurrent tasks", procsAvail));
         ExecutorService executor = Executors.newFixedThreadPool(procsAvail);
         IntStream.range(0, procsAvail).forEach( it -> executor.submit(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                fib(45);
-            }
+            heavyTask();
         }));
 
         if (!executor.awaitTermination(timeout, TimeUnit.SECONDS)) {
             println("Terminate stressing");
             executor.shutdownNow();
             if (!executor.awaitTermination(2, TimeUnit.SECONDS)) {
-                println("Could not terminate gracefully");
+                println("Could not terminate gracefully ");
             }
         }
         executor.shutdownNow();
         println("Finished.");
+    }
+
+    private void heavyTask() {
+        int millisToSleep = 10;
+        while (!Thread.currentThread().isInterrupted()) {
+            fib(40);
+            try {
+//                println(format("Sleep for %d ms", millisToSleep));
+                Thread.sleep(millisToSleep);
+            } catch (InterruptedException e) {
+//                println(format("%s is aborted, so break out of the loop", Thread.currentThread().getName()));
+                break;
+            }
+        }
     }
 
     /**
